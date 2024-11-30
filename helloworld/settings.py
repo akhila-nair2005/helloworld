ac16e4fcfa9d2992fd737d2458cb322e00411040
akhila-nair2005 <akhiladineshbabu2005@gmail.com>
2024-11-30 13:22:58 +0530

    Fire

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1=wrxm#2ztc=l9=!rntpe7ng^56fa-)%@1c$^@(i7bg-ew83db
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
